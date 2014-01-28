345193cbad83eaa0cc5b19f5a75e43637a2024a3
Jon Glick <jglick@basho.com>
2014-01-28 13:30:28 -0500

    Using riak_hostname instead of straight fqdn for configurability

--- puppet/modules/profile/manifests/riak/member.pp
+++ puppet/modules/profile/manifests/riak/member.pp
@@ -1,8 +1,8 @@
 class profile::riak::member (
 	$clusters = hiera('clusters', {}),
-	$riak_hostname
+	$riak_hostname = hiera('riak_hostname', $fqdn)
 ) {
-	$member_of = member_of_key($clusters, $fqdn)
+	$member_of = member_of_key($clusters, $riak_hostname)
 	include profile::riak::stepa
 	include profile::riak::stepb
 	notice("This node is a member of ${member_of}")
@@ -10,12 +10,13 @@ class profile::riak::member (
 		warning("Profile::Riak::Member added, but not found in any cluster")
     } elsif size($member_of) > 1 {
     	$csize = size($member_of)
-    	warning("Fqdn ${fqdn} found in ${csize} clusters: ${member_of}")
+    	warning("Fqdn ${riak_hostname} found in ${csize} clusters: ${member_of}")
 	} else {
 		$cluster = $member_of[0]
 		$sorted_list = sort($clusters[$cluster])
+		$fnode = $sorted_list[0]
 		notice("first member of cluster: ${sorted_list[0]}")
-		if $fqdn == $sorted_list[0] {
+		if $friak_hostname == $fnode {
 			notice ( "This node is the primary node" )
 			include profile::riak::stepd
 			Class['profile::riak::stepa'] -> 
